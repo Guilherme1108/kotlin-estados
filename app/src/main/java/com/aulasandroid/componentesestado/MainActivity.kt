32da985c2461b925195653c2da937facd627eda3
Guilherme Moreira <guilherme.moreira1108@gmail.com>
2026-03-04 10:51:25 -0300

    testando coisas de estilização e estados

--- app/src/main/java/com/aulasandroid/componentesestado/MainActivity.kt
+++ app/src/main/java/com/aulasandroid/componentesestado/MainActivity.kt
@@ -5,27 +5,57 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -82,34 +112,241 @@ fun BasicComponentsScreen(modifier: Modifier = Modifier) {
 @Composable
 fun TesteEstadosScreen(modifier: Modifier = Modifier) {
 
-    Column(modifier = modifier
-        .background(Color.Black)
-        .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+    var backgroundColor by remember {
+        mutableStateOf(Color(234, 158, 158, 255))
+    }
 
+    Column(modifier = modifier
+        .background(backgroundColor)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(14.dp)
     ) {
-        val nome = remember {
+        var nome by remember {
+            mutableStateOf("")
+        }
+
+        var idade by remember {
+            mutableStateOf("")
+        }
+
+        var email by remember {
+            mutableStateOf("batata@email")
+        }
+
+        var jCompose by remember {
+            mutableStateOf(false)
+        }
+
+        var xml by remember {
+            mutableStateOf(false)
+        }
+
+        var sOperacional by remember {
             mutableStateOf("")
         }
 
         TextField(
-            value = nome.value,
+            value = nome,
             onValueChange = { novoValor ->
-                nome.value = novoValor
+                nome = novoValor
+            },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words
+            ),
+            placeholder = {
+                Text(text = "Digite seu nome e sobrenome")
+            },
+            label = {
+                Text(text = "Nome completo")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Person Icon",
+                    tint = Color(100, 95, 173, 255)
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Person Icon",
+                    tint = Color(100, 95, 173, 255)
+                )
             }
         )
 
-        val idade = remember {
-            mutableStateOf("")
-        }
 
         TextField(
-            value = idade.value,
+            value = idade,
             onValueChange = { novoValor ->
-                idade.value = novoValor
+                idade = novoValor
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            placeholder = {
+                Text(text = "Digite sua idade")
+            },
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = Color(208, 13, 13, 255),
+                unfocusedTextColor = Color(27, 92, 215, 255),
+                unfocusedPlaceholderColor = Color(37, 236, 43, 255)
+            ),
+            shape = RoundedCornerShape(topStart = 20.dp, bottomEnd = 20.dp)
         )
+
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it},
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Email")
+            },
+            placeholder = {
+                Text(text = "Digite seu email")
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedTextColor = Color(208, 13, 13, 255),
+                unfocusedTextColor = Color(27, 92, 215, 255),
+                unfocusedPlaceholderColor = Color(37, 236, 43, 255),
+                focusedBorderColor = Color(245, 188, 43, 255),
+                unfocusedBorderColor = Color(250, 91, 17, 255)
+            )
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = jCompose,
+                onCheckedChange = { jCompose = it},
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.Green,
+                    uncheckedColor = Color.Red
+                )
+            )
+
+            Text(text = "Jetpack Compose")
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = xml,
+                onCheckedChange = { xml = it}
+            )
+
+            Text(text = "Jetpack Compose")
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = sOperacional == "android",
+                onClick = { sOperacional = "android" }
+            )
+
+            Text(text = "Android")
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = sOperacional == "ios",
+                onClick = { sOperacional = "ios" }
+            )
+
+            Text(text = "IOS")
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = sOperacional == "wPhone",
+                onClick = { sOperacional = "wPhone" }
+            )
+
+            Text(text = "Windows Phone")
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(
+                onClick = {
+                    backgroundColor = Color(142, 94, 208, 255)
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red,
+                    contentColor = Color.Black
+                ),
+                border = BorderStroke(width = 4.dp, Color.Cyan),
+                shape = RoundedCornerShape(topEnd = 10.dp, bottomStart = 10.dp)
+            ) {
+
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = "Star Icon"
+                )
+
+                Spacer(modifier = Modifier.width(4.dp))
+
+                Text(text = "Clique aqui")
+            }
+
+            OutlinedButton(
+                onClick = {
+                    backgroundColor = Color(222, 165, 80, 255)
+                }
+            ) {
+                Text(text = "Outro botão")
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            var favoritado by remember {
+                mutableStateOf(false)
+            }
+
+            if (favoritado ) {
+                Icon(
+                    modifier = Modifier.clickable{
+                        favoritado = false
+                    },
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "Favorite Icon",
+                )
+            } else {
+                Icon(
+                    modifier = Modifier.clickable{
+                        favoritado = true
+                    },
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = "Favorite border Icon"
+                )
+            }
+
+
+        }
+
 
     }
 
